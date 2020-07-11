f79ae0729fc64e064886a5c9b59a6d14d6254a29
Mugdha-Hazra <63424869+Mugdha-Hazra@users.noreply.github.com>
2020-07-11 20:35:02 +0530

    Add files via upload

---
+++ Function.cpp
@@ -0,0 +1,29 @@
+// Functions
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+/*
+Add `int max_of_four(int a, int b, int c, int d)` here.
+*/
+int max_of_four(int a, int b, int c, int d)
+{
+    if (a > b && a > c && a > d)
+        return a;
+    else if (b > a && b > c && b > d)
+        return b;
+    else if (c > b && c > a && c > d)
+        return c;
+    else
+        return d;
+}
+
+int main() {
+    int a, b, c, d;
+    cin>>a>>b>>c>>d;
+    int ans = max_of_four(a, b, c, d);
+    cout<<ans;
+
+    return 0;
+}
